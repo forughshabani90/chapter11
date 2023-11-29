a49606bcbdb793f4feaef9441e359c383ab63b0a
forughshabani90 <forugh.shabani.90@gmail.com>
2023-11-29 13:32:00 -0800

    sc

--- src/constructorchaining/Main.java
+++ src/constructorchaining/Main.java
@@ -4,6 +4,7 @@ import listing.GeometricObject;
 
 public class Main {
     public static void main(String[] args) {
+        // TODO: 11/29/2023  Refactor in code
         GeometricObject geometricObject= new GeometricObject();
 
     }
